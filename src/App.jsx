f6b6cdb0350c5b10b0955781b985d034a9b3aa96
horichii <virgilianvidal@gmail.com>
2026-03-26 12:06:00 +0800

    feat: added/modified main and app jsx files with the added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,59 @@
+import { useState } from 'react';
+import './styles/App.css';
+import General from './components/General';
+import { EducationalExperience } from './components/Educational';
+import Preview from './components/Preview';
+import WorkExperience from './components/Work';
 
-import './App.css'
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [cvData, setCv] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    schoolName: "",
+    program: "",
+    dateStudied: "",
+    companyName: "",
+    position: "",
+    responsibilities: "",
+    dateStart: "",
+    dateEnd:"",
+  });
+
+  const nextStep = () => setStep(step + 1);
+  const prevStep = () => setStep(step - 1);
   
+  const handleUpdate = (newData) => {
+    setCv({ ...cvData, ...newData });
+  };
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <div className="app-container">
+      <aside className="sidebar">
+        <h2 className='header'>CV Form</h2>
+        
+        {step === 1 && (
+          <General {...cvData} onUpdate={handleUpdate} onSubmit={nextStep} />
+        )}
+        
+        {step === 2 && (
+          <EducationalExperience {...cvData} onUpdate={handleUpdate} onSubmit={nextStep} onBack={prevStep} />
+        )}
+
+        {step === 3 && (
+          <WorkExperience {...cvData} onUpdate={handleUpdate} onSubmit={nextStep} onBack={prevStep} />
+        )}
+        
+        {step > 1 && <button onClick={prevStep} className="back-btn">Go back</button>}
+      </aside>
+
+      <main className="main-preview">
+        <Preview {...cvData} onUpdate={handleUpdate} onEdit={setStep}/>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
